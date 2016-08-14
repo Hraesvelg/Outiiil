0c4ce6df633d47151d2ed873ca99a545ff03860c
Hraesvelg <hetman.freddy@gmail.com>
2016-08-14 19:50:19 +0200

    mise à jour 1.4.1

--- content_scripts/page/Construction.js
+++ content_scripts/page/Construction.js
@@ -22,11 +22,36 @@ var PageConstruction = Page.extend({
 	initialize : function()
 	{
 		// Affichage de la rentabilité
-		var sacrifice = (21 + Utils.data.niveauConstruction[11]) * 40 * Math.pow(2, (Utils.data.niveauConstruction[11] + 3));
-		if(!$(".desciption_amelioration:eq(11) table").find(".verificationOK").length)
-			$(".desciption_amelioration:eq(11) table").append("<tr><td colspan=2>La construction est utile à partir de <span class='" + ((Utils.ouvrieres - Utils.terrain) > sacrifice ? "green" : "red") + "'>" + numeral(sacrifice).format() + "</span> ouvrières (disponible : " + numeral(Utils.ouvrieres - Utils.terrain).format() + ").</td></tr>");
+		if(!$(".desciption_amelioration:eq(11) table").find(".verificationOK").length) this.titleEtable();
+        // Sauvegarde construction
 		if(!Utils.comptePlus) this.plus();
 	},
+    /**
+    * Ajoute un title detaillé pour connaitre la rentabilité de la construction : etable à pucerons.
+    *
+    * @private
+    * @method titleEtable
+    * @return 
+    */
+    titleEtable : function(){
+        var ouvDispo = Utils.ouvrieres - Utils.terrain;
+        var capacite = ouvDispo * (10 + (Utils.data.niveauConstruction[11] / 2));
+        var perte = 80 * Math.pow(2, Utils.data.niveauRecherche[4]);
+        var capaciteS = (ouvDispo - perte) * (10 + ((Utils.data.niveauConstruction[11] + 1) / 2));
+        var seuil = (21 + Utils.data.niveauConstruction[11]) * 40 * Math.pow(2, (Utils.data.niveauConstruction[11] + 3));
+        
+        var title = "<table>" +
+            "<tr><td>Ouvrières</td><td class='right'>" + numeral(Utils.ouvrieres).format() + "</td></tr>" +
+            "<tr><td>Disponible</td><td class='right'>" +  numeral(ouvDispo).format() + "</td></tr>" +
+            "<tr><td>Capacité de livraison actuelle</td><td class='right'>" +  numeral(capacite).format() + "</td></tr>" +
+            "<tr><td>Perte ouvrières pour niveau " + (Utils.data.niveauConstruction[11] + 1) + "</td><td class='right'>" +  numeral(perte).format() + "</td></tr>" +
+            "<tr><td>Capacité de livraison niveau suivant</td><td class='right' style='padding-left:10px'>" +  numeral(capaciteS).format() + "</td></tr>" +
+            "<tr><td>Seuil rentabilité ouvrière</td><td class='right gras' style='padding-left:10px'>" +  numeral(seuil).format() + "</td></tr>" +
+            "</table>";
+            
+        $(".cout_amelioration:eq(11) table").prepend("<tr class='centre'><td colspan='2' id='o_rentabiliteEtable' title=''>Rentabilité</td></tr>");
+        $("#o_rentabiliteEtable").tooltip({position : {my : "right+15 center", at : "left center"}, content : title});
+    },
 	/**
 	* Ajoute les fonctionnalités du compte+. Sauvegarde la construction en cours.
     *
@@ -40,12 +65,8 @@ var PageConstruction = Page.extend({
 		if($("#centre > strong").length)
 			$("#centre > strong").after("<span class='small'> Terminé le " + moment().add($("#centre > strong").text().split(',')[0].split('(')[1], 's').format("D MMM YYYY à HH[h]mm") + "</span>");
 		// Sauvegarde de la construction en cours
-		var str = $("#centre > strong").text();
-		var construction = str.substring(2, str.indexOf("se termine") - 1);
-		if(construction && (!Utils.data.construction || moment().diff(moment(Utils.data.expConstruction), 's') > 0)){
-			var time = parseInt(str.split(',')[0].split('(')[1]);
-			this.saveConstruction(construction, time);
-		}
+        this.saveConstruction();
+        // Suppresion de la construction en cours si on annule
 		if($("a:contains('Annuler')").length)
 			$("a:contains('Annuler')").click(function(){
 				Utils.data.expConstruction = -1;
@@ -59,17 +80,18 @@ var PageConstruction = Page.extend({
     *
 	* @private
 	* @method saveConstruction
-	* @param {String} construction
-	* @param {Integer} temps en secondes
 	* @return 
 	*/
-	saveConstruction : function(construction, temps)
+	saveConstruction : function()
 	{
-		var tmp = construction.substr(0,1).toUpperCase() + construction.substr(1);
-		Utils.data.expConstruction = moment().add(temps, 's');
-		Utils.data.startConstruction = moment();
-		Utils.data.construction = construction;
-		localStorage.setItem("outiiil_data", JSON.stringify(Utils.data));
-		if(!Utils.comptePlus && $("#boiteComptePlus").length) boiteComptePlus.majConstruction();
+        var str = $("#centre > strong").text();
+		var construction = str.substring(2, str.indexOf("se termine") - 1);
+		if(construction && (!Utils.data.construction || moment().diff(moment(Utils.data.expConstruction), 's') > 0)){
+            Utils.data.expConstruction = moment().add(parseInt(str.split(',')[0].split('(')[1]), 's');
+            Utils.data.startConstruction = moment();
+            Utils.data.construction = construction.substr(0,1).toUpperCase() + construction.substr(1);
+            localStorage.setItem("outiiil_data", JSON.stringify(Utils.data));
+            if(!Utils.comptePlus && $("#boiteComptePlus").length) boiteComptePlus.majConstruction();
+		}
 	}
 });
